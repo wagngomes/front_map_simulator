9b14972f16627efeac3511d342b91fa1c1354055
wagngomes <wagner.gomes.cx2@gmail.com>
2025-02-16 19:51:50 -0300

    feat: fectch map

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,59 @@
+"use client"
+
 import { DataTable } from "./saldos/data-table";
 import { columns } from "./saldos/columns";
-//import axios from "axios";
+import { useEffect, useState } from "react";
 
-const data = [
-  {
-    produto: "xxxxxxx",
-    descricao: "xxxxxxx",
-    b_o: "xxxxxxx",
-    mat_med: "xxxxxxx",
-    fornecedor: "xxxxxxx",
-    comprador: "xxxxxxx",
-    primeira_filial_rota: "xxxxxxx",
-    filial_dest: "xxxxxxx",
-    sigla_dest: "xxxxxxx",
-    empresa_dest: "xxxxxxx",
-    grupo_tributacao: "xxxxxxx",
-    flag_tributacao: "xxxxxxx",
-    rota: "xxxxxxx",
-    codigo: "xxxxxxx",
-    gui_semana_saida: "xxxxxxx",
-    gui_semana_chegada: "xxxxxxx",
-    modo_compra: "xxxxxxx",
-    dia_sugerido_pedido: "xxxxxxx",
-    qtd_comprada: 200,
-    preco_total: 1000,
-    status: "xxxxxxx",
-  },
-];
-
-/*const data = axios.get("http://localhost:3002/saldos").then((response) => {
-  return response;
-});*/
+interface MapData {
+  produto: string;
+  descricao: string;
+  b_o: string;
+  mat_med: string;
+  fornecedor: string;
+  comprador: string;
+  primeira_filial_rota: string;
+  filial_dest: string;
+  sigla_dest: string;
+  empresa_dest: string;
+  grupo_tributacao: string;
+  flag_tributacao: string;
+  rota: string;
+  codigo: string;
+  gui_semana_saida: string;
+  gui_semana_chegada: string;
+  modo_compra: string;
+  dia_sugerido_pedido: string;
+  qtd_comprada: number;
+  preco_total: number;
+  status: string;
+}
 
 export default function Home() {
+
+  const [data, setData] = useState<MapData[]>([]); // ou o tipo específico dos seus dados
+
+  // Função para buscar os dados
+  async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:3002/completeMap');
+      const data = await response.json();
+
+      // Garantir que os dados sejam do tipo MapData[]
+      const typedData: MapData[] = data as MapData[]; // Type assertion (casting)
+
+      setData(typedData); // Atualiza o estado com os dados tipados
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+    }
+  }
+
+  // Chama a função fetchData quando o componente é montado
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
   return (  
     <>
     <div className="flex justify-center flex-col m-4">
